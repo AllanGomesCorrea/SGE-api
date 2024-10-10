5bd545d2072d6242c4e34629fb11e938200826a1
allan <allan.correa-ext@rtm.net.br>
2024-10-10 09:53:00 -0300

    Add CORS to true in settings.

--- app/settings.py
+++ app/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'rest_framework_simplejwt',
+    
+    'corsheaders',
 
     'authentication',
     'brands',
@@ -65,7 +67,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True  # Permite qualquer origem (use com cuidado em produção)
 
 ROOT_URLCONF = 'app.urls'
 
